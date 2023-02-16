5d1c4819a06678cc4bc541c763ad39c717111d66
Sara Lee <leesara502@gmail.com>
2023-02-15 22:53:27 -0500

    first commit

---
+++ knowledge_check_1.py
@@ -0,0 +1,7 @@
+print("hello world!")
+list_animals = ['fossa', 'leafy seadragon', 'pink fairy armadillo', 'okapi']
+print(list_animals[1])
+dictionary_french = {'oui': 'yes', 'non': 'no'}
+print(dictionary_french['oui'])
+tuple_cats = ('Maine Coon', 'Sphynx', 'Ragdoll', 'OneOrangeBraincell')
+print(tuple_cats[3])
